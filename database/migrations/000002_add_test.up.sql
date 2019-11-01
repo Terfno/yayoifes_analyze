a85f00d71d201a6e1b285b04c811522835c9cc3d
terfno <sueda.takahito@gmail.com>
2019-11-02 01:08:57 +0900

    :bug: log->logs を力技で対応

--- database/migrations/000002_add_test.up.sql
+++ database/migrations/000002_add_test.up.sql
@@ -1,3 +1,3 @@
-INSERT INTO log (age , sex , timing ) VALUES(1,'man','2019-11-02 0:17:35');
-INSERT INTO log (age , sex , timing ) VALUES(1,'woman','2019-11-02 0:18:35');
-INSERT INTO log (age , sex , timing ) VALUES(1,'other','2019-11-02 0:19:35');
+INSERT INTO logs (age , sex , timing ) VALUES(1,'man','2019-11-02 0:17:35');
+INSERT INTO logs (age , sex , timing ) VALUES(1,'woman','2019-11-02 0:18:35');
+INSERT INTO logs (age , sex , timing ) VALUES(1,'other','2019-11-02 0:19:35');
