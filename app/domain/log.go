40927abb14308b0f5276aaf23e6fa69ec3fa8e05
terfno <sueda.takahito@gmail.com>
2019-11-02 00:09:52 +0900

    :tada: define domain

--- app/domain/log.go
+++ app/domain/log.go
@@ -1,2 +1,14 @@
 package domain
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type Log struct {
+	gorm.Model
+	id     int       `json:id`
+	age    int       `json:age`
+	sex    string    `json:sex`
+	timing time.Time `json:datetime`
+}
