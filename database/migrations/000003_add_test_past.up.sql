558b91d60f5dd6f77bacd7a6f9c5cc22bde26e61
terfno <sueda.takahito@gmail.com>
2019-11-03 02:30:00 +0900

    :fire: add test case

---
+++ database/migrations/000003_add_test_past.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO logs (age , sex , timing ) VALUES(2,'man','2019-11-02 12:17:35');
+INSERT INTO logs (age , sex , timing ) VALUES(2,'woman','2019-11-02 12:18:35');
+INSERT INTO logs (age , sex , timing ) VALUES(2,'other','2019-11-02 12:19:35');
