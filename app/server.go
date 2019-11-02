564f56be79241c84436031019b674a52b926376a
terfno <sueda.takahito@gmail.com>
2019-11-02 22:01:01 +0900

    :tada: hello ui

--- app/server.go
+++ app/server.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"net/http"
+	"path/filepath"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/multitemplate"
 
 	"github.com/Terfno/yayoifes_analyze/app/handler"
 )
 
 func main() {
 	r := gin.Default()
+	r.HTMLRender = loadTemplates("./templates")
 
 	// try
 	r.GET("/hello", handler.Hello)
 	r.GET("/dbtest", handler.DBtest)
 
+	// ui
+	r.GET("/",func(c *gin.Context){
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
 	// api
 	apiRouter := r.Group("/api")
 	{
-		apiRouter.GET("/numofvisitor",handler.GetNumberOfVisitor)
+		apiRouter.GET("/nov",handler.GetNumberOfVisitor)
 		apiRouter.GET("/novperhour",handler.GetNumberOfVisitorPerHour)
 	}
 
 	r.Run(":8080")
+}
+
+func loadTemplates(templatesDir string) multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	layouts, err := filepath.Glob(templatesDir + "/layouts/*.html")
+	if err != nil {
+		log.Fatal("html load err")
+	}
+
+	includes, err := filepath.Glob(templatesDir + "/includes/*.html")
+	if err != nil {
+		log.Fatal("html load err")
+	}
+
+	// Generate our templates map from our layouts/ and includes/ directories
+	for _, include := range includes {
+		layoutCopy := make([]string, len(layouts))
+		copy(layoutCopy, layouts)
+		files := append(layoutCopy, include)
+		r.AddFromFiles(filepath.Base(include), files...)
+	}
+	return r
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/Terfno/yayoifes_analyze
 
 require (
+	github.com/gin-contrib/multitemplate v0.0.0-20190914010127-bba2ccfe37ec // indirect
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.11
 	github.com/kr/pretty v0.1.0 // indirect
