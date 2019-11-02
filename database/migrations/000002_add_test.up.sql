4f55294a579111664f09e4c4e4bfc1b77b84aa94
terfno <sueda.takahito@gmail.com>
2019-11-03 03:01:48 +0900

    :zap: add more test case

--- database/migrations/000002_add_test.up.sql
+++ database/migrations/000002_add_test.up.sql
@@ -1,3 +1,4 @@
 INSERT INTO logs (age , sex , timing ) VALUES(1,'man','2019-11-02 0:17:35');
 INSERT INTO logs (age , sex , timing ) VALUES(1,'woman','2019-11-02 0:18:35');
 INSERT INTO logs (age , sex , timing ) VALUES(1,'other','2019-11-02 0:19:35');
+INSERT INTO logs (age , sex , timing ) VALUES(1,'other','2019-11-02 5:19:35');
---
+++ database/migrations/000004_.up.sql
@@ -0,0 +1,1 @@
+INSERT INTO logs (age , sex , timing ) VALUES(1,'other','2019-11-03 2:43:17');
